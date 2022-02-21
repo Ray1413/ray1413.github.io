59cd368b3aa5a17171de0096cbc0a75b593fcab6
ray472809026 <ray472809026@hotmail.com>
2022-02-21 12:26:41 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/36XXvpd7Z2RxfX72CZqkY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/180-955d66ab2299b8d5.js","static/chunks/pages/index-8a1702b17b753f8e.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
